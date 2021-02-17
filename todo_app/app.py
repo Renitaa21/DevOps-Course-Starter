3f7d076217384f5e39a524981745ee55f1ee9c44
Renita Furtado <renita.furtado@uk.zurich.com>
2021-02-17 18:19:59 +0000

    Display saved items

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,10 +5,15 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+todoitems = [
+    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  
+    return render_template('index.html', todoitems = todoitems)
 
 
 if __name__ == '__main__':
