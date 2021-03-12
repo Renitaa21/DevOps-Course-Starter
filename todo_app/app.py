d743e4dbd13bd0b886a0a683139b4174242a49fa
Renita Furtado <renita.furtado@uk.zurich.com>
2021-03-12 21:47:51 +0000

    Call the Trello API , Add items and move to Done

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,6 +3,8 @@ from werkzeug.utils import redirect
 from todo_app.data.session_items import *
 from todo_app.flask_config import Config
 from flask import request 
+import requests
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,17 +13,30 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    todoitems = get_items()
+    todoitemsresponse = requests.get("https://api.trello.com/1/lists/6047ecb966a18d23e8029cd2/cards?&key="+os.environ.get('API_KEY')+"&token="+os.environ.get('TOKEN_KEY')).json()
+    todoitems = []
+    for obj in todoitemsresponse:
+        cardname = obj['name']
+        todoitems.append(cardname)
     return render_template('index.html', todoitems = todoitems)
 
 @app.route('/add', methods=['POST'])
 def add():
     if request.method == 'POST':
-        add_item(title=request.form.get('title'))
+        URL = "https://api.trello.com/1/cards?&key="+os.environ.get('API_KEY')+"&token="+os.environ.get('TOKEN_KEY')+"&idList=6047ecb966a18d23e8029cd2&name=" + request.form.get('title')
+        requests.post(URL)
         return redirect("http://127.0.0.1:5000/")
     else:
         pass
     
+@app.route('/move', methods=['POST'])
+def move():
+    if request.method == 'POST':
+        URL = "https://api.trello.com/1/cards/"+request.form.get('id')+"?&key="+os.environ.get('API_KEY')+"&token="+os.environ.get('TOKEN_KEY')+"&idList=6047ecb966a18d23e8029cd4"
+        requests.put(URL)
+        return redirect("http://127.0.0.1:5000/")
+    else:
+        pass
 
 if __name__ == '__main__':
     app.run()
