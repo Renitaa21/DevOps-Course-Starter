8a3920b7544b4178938c56cef01f9f3ed89f977d
Renita Furtado <renita.furtado@uk.zurich.com>
2021-05-11 16:58:04 +0100

    Unit tests working

--- todo_app/test_sample.py
+++ todo_app/test_sample.py
@@ -10,16 +10,38 @@ from todo_app.item import Item
 #         {'id':3,'status': 'Completed', 'title': 'List saved completed items'}
 #     ]
 
-
-
-
 def test_doneitems():
     items = [
-        Item(1, 'List saved todo items', 'Todo'),
+        Item(1, 'List saved todo items', 'To Do'),
         Item(2, 'List saved doing items', 'Doing'),
         Item(3, 'List saved completed items', 'Completed')
     ]
 
     view_model = ViewModel(items)
+    
+    assert len(view_model.completed_items()) == 1
 
-    assert view_model.completed_items().count == 1
+
+def test_todoitems():
+    items = [
+        Item(1, 'List saved todo items', 'To Do'),
+        Item(2, 'List saved doing items', 'Doing'),
+        Item(3, 'List saved completed items', 'Completed')
+    ]
+
+    view_model = ViewModel(items)
+    
+    assert len(view_model.todo_items()) == 1
+
+
+def test_doingitems():
+    items = [
+        Item(1, 'List saved todo items', 'To Do'),
+        Item(2, 'List saved doing items', 'Doing'),
+        Item(3, 'List saved completed items', 'Completed'),
+        Item(4, 'List saved doing items 2', 'Doing')
+    ]
+
+    view_model = ViewModel(items)
+    
+    assert len(view_model.doing_items()) == 2
