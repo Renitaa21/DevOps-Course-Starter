ecffffdb310241de0f36c9d2057ca3fd0cfa1797
Renita Furtado <renita.furtado@uk.zurich.com>
2021-05-13 13:13:25 +0100

    Integrations tests - working Updated JSON

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,73 +9,73 @@ import pdb
 import json
 from todo_app.view_model import ViewModel
 from todo_app.item import Item
-class App:
-    def create_app(self):
-        app = Flask(__name__)
-        app.config.from_object('todo_app.flask_config.Config')
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('todo_app.flask_config.Config')
+    
+    @app.route('/')
+    def index():
+        query_params = {
+            "key": os.environ.get('API_KEY'),
+            "token": os.environ.get('TOKEN_KEY')
+        }  
+        url = f"https://api.trello.com/1/lists/{os.environ.get('TODO_LISTID')}/cards"
+        itemsresponse = requests.get(url, params = query_params).json()
+        items = []
+        for card in itemsresponse:
+            myitem = Item.from_trello_card(card)
+            items.append(myitem)
+        view_model = ViewModel(items)
+        return render_template('index.html',view_model=view_model)
+
+    @app.route('/add', methods=['POST'])
+    def add():
+        query_params = {
+            "key": os.environ.get('API_KEY'),
+            "token": os.environ.get('TOKEN_KEY'),
+            "idList": os.environ.get('TODO_LISTID'),
+            "name": request.form.get('title')
+        }
+        url = "https://api.trello.com/1/cards"
+        requests.post(url, params=query_params)
+        return redirect(url_for("index"))
         
-        @app.route('/')
-        def index():
-            query_params = {
-                "key": os.environ.get('API_KEY'),
-                "token": os.environ.get('TOKEN_KEY')
-            }  
-            url = f"https://api.trello.com/1/lists/{os.environ.get('TODO_LISTID')}/cards"
-            itemsresponse = requests.get(url, params = query_params).json()
-            items = []
-            for card in itemsresponse:
-                myitem = Item.from_trello_card(card)
-                items.append(myitem)
-            view_model = ViewModel(items)
-            return render_template('index.html',view_model=view_model)
-
-        @app.route('/add', methods=['POST'])
-        def add():
-            query_params = {
-                "key": os.environ.get('API_KEY'),
-                "token": os.environ.get('TOKEN_KEY'),
-                "idList": os.environ.get('TODO_LISTID'),
-                "name": request.form.get('title')
-            }
-            url = "https://api.trello.com/1/cards"
-            requests.post(url, params=query_params)
-            return redirect(url_for("index"))
+    @app.route('/complete_item', methods=['POST'])
+    def complete_item():
             
-        @app.route('/complete_item', methods=['POST'])
-        def complete_item():
-                
-            url = f"https://api.trello.com/1/cards/{request.form.get('id')}"
-            query_params = {
-                "key": os.environ.get('API_KEY'),
-                "token": os.environ.get('TOKEN_KEY'),
-                "idList": os.environ.get('DONE_LISTID')
-            }  
-            requests.put(url, params=query_params)
-                
-            return redirect(url_for("index"))
-
-
-        @app.route('/completeitem/<id>', methods=['GET','POST'])
-        def completeitem(id):
-            url = f"https://api.trello.com/1/cards/{id}"
-            query_params = {
-                "key": os.environ.get('API_KEY'),
-                "token": os.environ.get('TOKEN_KEY'),
-                "idList": os.environ.get('DONE_LISTID')
-            }
-
-            requests.put(url, params=query_params)
+        url = f"https://api.trello.com/1/cards/{request.form.get('id')}"
+        query_params = {
+            "key": os.environ.get('API_KEY'),
+            "token": os.environ.get('TOKEN_KEY'),
+            "idList": os.environ.get('DONE_LISTID')
+        }  
+        requests.put(url, params=query_params)
             
-            return redirect(url_for("index"))
+        return redirect(url_for("index"))
 
-        if __name__ == '__main__':
-            app.run()
 
-        _DEFAULT_ITEMS = [
-                {'id':1,'status': 'Not Started', 'title': 'List saved todo items'},
-                {'id':2,'status': 'Not Started', 'title': 'List saved doing items'},
-                {'id':3,'status': 'Completed', 'title': 'List saved completed items'}
-            ]
-        return app
+    @app.route('/completeitem/<id>', methods=['GET','POST'])
+    def completeitem(id):
+        url = f"https://api.trello.com/1/cards/{id}"
+        query_params = {
+            "key": os.environ.get('API_KEY'),
+            "token": os.environ.get('TOKEN_KEY'),
+            "idList": os.environ.get('DONE_LISTID')
+        }
+
+        requests.put(url, params=query_params)
+        
+        return redirect(url_for("index"))
+
+    if __name__ == '__main__':
+        app.run()
+
+    _DEFAULT_ITEMS = [
+            {'id':1,'status': 'Not Started', 'title': 'List saved todo items'},
+            {'id':2,'status': 'Not Started', 'title': 'List saved doing items'},
+            {'id':3,'status': 'Completed', 'title': 'List saved completed items'}
+        ]
+    return app
 
 
--- todo_app/test_integrate.py
+++ todo_app/test_integrate.py
@@ -1,37 +1,70 @@
-from todo_app.app import App
-from unittest import mock
-from unittest.mock import patch
+from todo_app import app
+from unittest.mock import patch,Mock
 import pytest
 from dotenv import load_dotenv,find_dotenv
-import requests
 import os
-
+#import requests
 
 @pytest.fixture
-def client():
+def test_client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
-    app = App()
     test_app = app.create_app()
     with test_app.test_client() as client:
         yield client
 
 @patch('requests.get')
-def test_index_page(mock_get_requests, client):
+def test_index_page(mock_get_requests, test_client):
     # Replace call to requests.get(url) with our own function
+    #Arrange
     query_params = {
                 "key": os.environ.get('API_KEY'),
                 "token": os.environ.get('TOKEN_KEY')
             }  
     url = "https://api.trello.com/1/boards/123/lists"
+    #Act
     mock_get_requests.side_effect = mock_get_lists(url, params=query_params)
-    response = client.get('/')
+    response = test_client.get('/')
+    #assert needs to go here 
+    assert response.status_code == 200
 
+#not a test just a function
 def mock_get_lists(url, params):
-    if url == f"https://api.trello.com/1/boards/123/lists":
-        response = mock()
+    if url == f"https://api.trello.com/1/boards/{os.environ.get('TESTBOARDID')}/lists":
+        response = Mock()
         # sample_trello_lists_response should point to some test response data
-        sample_trello_lists_response = " blahblahModifytobehavelikejson"
+        sample_trello_lists_response = [
+    {
+        "id": "6047ecb966a18d23e8029cd2",
+        "name": "To Do",
+        "closed": False,
+        "pos": 16384,
+        "softLimit": None,
+        "idBoard": "6047ecb966a18d23e8029cd1",
+        "subscribed": False
+    },
+    {
+        "id": "6047ecb966a18d23e8029cd3",
+        "name": "Doing",
+        "closed": False,
+        "pos": 32768,
+        "softLimit": None,
+        "idBoard": "6047ecb966a18d23e8029cd1",
+        "subscribed": False
+    },
+    {
+        "id": "6047ecb966a18d23e8029cd4",
+        "name": "Done",
+        "closed": False,
+        "pos": 49152,
+        "softLimit": None,
+        "idBoard": "6047ecb966a18d23e8029cd1",
+        "subscribed": False
+    }
+]
+
         response.json.return_value = sample_trello_lists_response
         return response
     return None
+
+#Set null to None, true to True, false to False, get valid response and add that in sample_trello_lists_response 
